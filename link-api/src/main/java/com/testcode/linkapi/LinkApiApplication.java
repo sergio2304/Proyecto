67f9071d2c7a7eb6322b03e6bf578e0a03b70345
Sergio Aguirre <aguirre.martinez.sergior@gmail.com>
2021-12-18 17:42:51 -0500

    ProyectoInicial

---
+++ link-api/src/main/java/com/testcode/linkapi/LinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.testcode.linkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkApiApplication.class, args);
+	}
+
+}
---
+++ link-api/src/test/java/com/testcode/linkapi/LinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcode.linkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
