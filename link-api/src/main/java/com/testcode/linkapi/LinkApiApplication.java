d327b9a18564aa707c4446348897a765516b0e6e
Sergio Aguirre <aguirre.martinez.sergior@gmail.com>
2021-12-18 19:06:01 -0500

    se añade mapper

--- link-api/src/main/java/com/testcode/linkapi/LinkApiApplication.java
+++ link-api/src/main/java/com/testcode/linkapi/LinkApiApplication.java
@@ -2,9 +2,16 @@ package com.testcode.linkapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LinkApiApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkApiApplication.class, args);
